0c728c9373114575e1d0fc4a6b8e8241b9d73f7d
mdsohel-17 <sohelmd01494md@gmail.com>
2024-12-05 09:55:06 +0530

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,36 +5,37 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Header = () => {
   return (
-    <div className="w-100 flex flex-wrap px-20 justify-between items-center bg-white border-b py-2">
-      <div className="w-1/12 flex flex-col items-center">
-        <img src={logo} alt="" />
-        <div className=" text-sm font-medium text-[#af601a] ">
-          WILD COLLECTION
-        </div>
+    <div className="w-100 h-[10vh] flex flex-wrap px-20 justify-between items-center bg-white shadow-lg border-b anim py-2 sticky top-0 z-50">
+      <div className="w-2/12 flex items-center">
+        <img src={logo} alt="" className="w-3/12" />
+        <span className=" text-lg leading-[20px] font-mono font-medium text-[#af601a] ">
+          TrailBliss
+          <div className="ms-14">Bags</div>
+        </span>
       </div>
-      <div className="w-7/12 cursor-pointer">
-        <ul className="list-none flex justify-around">
-          <li className="text-lg flex hover:text-[#af601a] font-medium relative after:absolute hover:after:absolute hover:after:content-[''] after:w-0 after:rounded-lg after:h-[2.5px] after:bg-[#af601a] after:top-6 hover:after:w-full">
+      <div className="w-5/12 cursor-pointer">
+        <ul className="list-none flex justify-around ">
+          <li className="text-lg flex text-[#0b0f12a3] hover:text-[#af601a] font-medium relative after:absolute hover:after:absolute hover:after:content-[''] after:w-0 after:rounded-lg after:h-[2.5px] after:bg-[#af601a] after:top-6 hover:after:w-full">
             Home
           </li>
-          <li className="text-lg flex font-medium hover:text-[#af601a] relative after:absolute hover:after:absolute hover:after:content-[''] after:w-0 after:rounded-lg after:h-[2.5px] after:bg-[#af601a] after:top-6 hover:after:w-full ">
+          <li className="text-lg flex text-[#0b0f12a3] font-medium hover:text-[#af601a] relative after:absolute hover:after:absolute hover:after:content-[''] after:w-0 after:rounded-lg after:h-[2.5px] after:bg-[#af601a] after:top-6 hover:after:w-full ">
             New Release
           </li>
-          <li className="text-lg flex font-medium hover:text-[#af601a] relative after:absolute hover:after:absolute hover:after:content-[''] after:w-0 after:rounded-lg after:h-[2.5px] after:bg-[#af601a] after:top-6 hover:after:w-full">
+          <li className="text-lg flex text-[#0b0f12a3] font-medium hover:text-[#af601a] relative after:absolute hover:after:absolute hover:after:content-[''] after:w-0 after:rounded-lg after:h-[2.5px] after:bg-[#af601a] after:top-6 hover:after:w-full">
             Product
           </li>
-          <li className="text-lg flex font-medium hover:text-[#af601a] relative after:absolute hover:after:absolute hover:after:content-[''] after:w-0 after:rounded-lg after:h-[2.5px] after:bg-[#af601a] after:top-6 hover:after:w-full">
+          <li className="text-lg flex text-[#0b0f12a3] font-medium hover:text-[#af601a] relative after:absolute hover:after:absolute hover:after:content-[''] after:w-0 after:rounded-lg after:h-[2.5px] after:bg-[#af601a] after:top-6 hover:after:w-full">
             Gallery
           </li>
-          <li className="text-lg flex font-medium hover:text-[#af601a] relative after:absolute hover:after:absolute hover:after:content-[''] after:w-0 after:rounded-lg after:h-[2.5px] after:bg-[#af601a] after:top-6 hover:after:w-full">
+          <li className="text-lg flex text-[#0b0f12a3] font-medium hover:text-[#af601a] relative after:absolute hover:after:absolute hover:after:content-[''] after:w-0 after:rounded-lg after:h-[2.5px] after:bg-[#af601a] after:top-6 hover:after:w-full">
             About
           </li>
-          <li className="text-lg flex font-medium hover:text-[#af601a] relative after:absolute hover:after:absolute hover:after:content-[''] after:w-0 after:rounded-lg after:h-[2.5px] after:bg-[#af601a] after:top-6 hover:after:w-full">
+          <li className="text-lg flex text-[#0b0f12a3] font-medium hover:text-[#af601a] relative after:absolute hover:after:absolute hover:after:content-[''] after:w-0 after:rounded-lg after:h-[2.5px] after:bg-[#af601a] after:top-6 hover:after:w-full">
             Contact
           </li>
         </ul>
       </div>
-      <div className="w-3/12">
+      <div className="w-3/12 border">
         <FontAwesomeIcon
           icon={faMagnifyingGlass}
           style={{ color: "#af601a" }}
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -11,12 +11,84 @@ import "swiper/css/navigation";
 // import required modules
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import { slider1 } from "../utils/utils";
+import bag from "../assets/image/bag111.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowTrendUp, faShop } from "@fortawesome/free-solid-svg-icons";
 
 const Slider = () => {
   return (
     <>
-      <div className="bg-slate-500 h-[90vh]">
-        <Swiper
+      <div className="h-[90vh] px-20">
+        <div className="flex flex-wrap h-full -mx-3">
+          <div className="w-full px-2">
+            <div className=" text-center pt-10">
+              <h1 className="text-5xl font-medium px-36 leading-[65px]">
+                TrailBliss Bags Brings You High-Quality Bags Designed to Fit
+                Your Everyday Life.
+              </h1>
+              <p className="mt-3 font-medium">
+                TrailBliss Bags offers stylish, durable, and versatile bags for
+                every occasion. Carry comfort and quality wherever you go.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-wrap justify-between px-2 pb-10 items-end">
+            <div className="w-2/12 px-3">
+              <div className=" py-16 shadow-lg shadow-[#1c150f4a] bg-[#af601a] rounded-3xl ">
+                <img
+                  src={bag}
+                  alt=""
+                  className="hover:scale-150 transition-all duration-1000"
+                />
+              </div>
+            </div>
+            <div className="w-2/12 px-3">
+              <div className=" p-4 drop-shadow-xl shadow-xl  border-[#af601a] bg-[#af601a] rounded-3xl hover:scale-110 transition-all duration-1000">
+                <div className=" bg-white rounded-full w-12 h-12 flex justify-center items-center  ">
+                  <FontAwesomeIcon
+                    icon={faArrowTrendUp}
+                    style={{ color: "#af601a" }}
+                    size="xl"
+                  />
+                </div>
+                <h1 className="mt-3 text-white font-medium text-4xl">100+</h1>
+                <p className="text-white py-4 text-xl ">
+                  Bags Sold Every Week, Bringing Quality, Style, and Versatility
+                  to Our Customers Every Single Day!
+                </p>
+              </div>
+            </div>
+            <div className="w-4/12 px-3">
+              <div className=" py-10 drop-shadow-xl shadow-xl bg-[#d7dbdd42] rounded-3xl"></div>
+            </div>
+            <div className="w-2/12 px-3">
+              <div className="p-4 drop-shadow-xl shadow-xl  border-[#af601a] bg-[#af601a] rounded-3xl hover:scale-110 transition-all duration-1000">
+                <div className=" bg-white rounded-full w-12 h-12 flex justify-center items-center">
+                  <FontAwesomeIcon
+                    icon={faShop}
+                    style={{ color: "#af601a" }}
+                    size="xl"
+                  />
+                </div>
+                <h1 className="mt-3 text-white font-medium text-4xl">60+</h1>
+                <p className="text-white py-4 pb-11 text-xl ">
+                  New Designs Every Month to Keep Your Style Fresh and Exciting!
+                </p>
+              </div>
+            </div>
+
+            <div className="w-2/12 px-3">
+              <div className=" py-16  shadow-lg group-[]: shadow-[#1c150f4a]  bg-[#af601a] rounded-3xl">
+                <img
+                  src={bag}
+                  alt=""
+                  className="hover:scale-150 transition-all duration-1000"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <Swiper
           spaceBetween={30}
           centeredSlides={true}
           autoplay={{
@@ -34,7 +106,7 @@ const Slider = () => {
             <img src={slider1} alt="" className=" w-full h-[90vh] " />
           </SwiperSlide>
           <SwiperSlide>Slide 2</SwiperSlide>
-        </Swiper>
+        </Swiper> */}
       </div>
     </>
   );
